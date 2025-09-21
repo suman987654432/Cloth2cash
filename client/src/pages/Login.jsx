8bc28d2068576c5fc3b432afd59e89cfa47176d1
Suman Kumar <sumanqaj9876@gmail.com>
2025-09-21 17:04:23 +0530

    fix: Correct model import case for User in user.js and routes refactor: Update login API endpoint to use production URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://cloth2cash.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
